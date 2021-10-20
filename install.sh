0a7419a65103c7385ba0592777c9a90baf2eb0fb
mateusz.kowalke <mateusz.kowalke@aurafutures.com>
2021-10-20 13:29:02 +0200

    fix: fixed search for os type

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if cat /etc/os-release | grep "Ubuntu" > /dev/null; then
+if cat /etc/os-release | grep -i "ubuntu" > /dev/null; then
     echo "detected ubuntu system"
     sudo apt update
     sudo apt install stow i3 i3status tmux
-elif cat /etc/os-release | grep "Arch" > /dev/null; then
+elif cat /etc/os-release | grep -i "arch" > /dev/null; then
     echo "detected arch system"
     sudo pacman -S stow i3 i3status tmux
 fi
