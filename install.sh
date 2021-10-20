20b5d5059b9317fdf35db01745c39084992c4cd5
mateusz.kowalke <mateusz.kowalke@aurafutures.com>
2021-10-20 13:47:45 +0200

    feature: added removing of arch conf files after install

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 if cat /etc/os-release | grep -i "ubuntu" > /dev/null; then
     echo "detected ubuntu system"
     sudo apt update
-    sudo apt install stow i3 i3status tmux
+    sudo apt install stow i3-wm i3status tmux
 elif cat /etc/os-release | grep -i "arch" > /dev/null; then
     echo "detected arch system"
-    sudo pacman -S stow i3 i3status tmux
+    sudo pacman -S stow i3-wm i3status tmux
+    rm ~/.config/i3/config
+    rm ~/.config/i3status/config
 fi
