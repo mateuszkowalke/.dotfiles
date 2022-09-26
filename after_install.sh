2cdcffd566cdcb696961f68e9a90671a5359f184
mateuszkowalke <mateusz.kowalke@gmail.com>
2022-09-26 23:03:02 +0200

    up

--- after_install.sh
+++ after_install.sh
@@ -9,8 +9,9 @@ sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 
 # update and install necessary packages
+# ripgrep is needed for nvim telescope to work properly
 sudo apt update
-sudo apt install -y apt-transport-https build-essential zsh stow fzf pip tmux lm-sensors brave-browser pgadmin4
+sudo apt install -y apt-transport-https build-essential zsh stow fzf pip tmux lm-sensors brave-browser pgadmin4 ripgrep
 
 # install pyenv's dependencies
 sudo apt-get install git python-pip make build-essential libssl-dev zlib1g-dev \
