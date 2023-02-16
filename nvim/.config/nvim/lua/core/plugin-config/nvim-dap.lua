3293cd946667af9c66d09f4657ce3687f89e17c8
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2023-02-16 13:16:15 +0100

    fix: fixed dap config to disconnect correctly and close dapui

--- nvim/.config/nvim/lua/core/plugin-config/nvim-dap.lua
+++ nvim/.config/nvim/lua/core/plugin-config/nvim-dap.lua
@@ -15,7 +15,7 @@ dapui.setup()
 
 -- debugging keymaps
 vim.keymap.set('n', '<leader>ds', ":lua require'dap'.continue()<CR>")
-vim.keymap.set('n', '<leader>dk', ":lua require'dap'.terminate()<CR>")
+vim.keymap.set('n', '<leader>dk', ":lua require'dap'.disconnect({ terminateDebuggee = true })<CR>")
 vim.keymap.set('n', '<leader>dt', ":lua require'dap'.toggle_breakpoint()<CR>")
 vim.keymap.set('n', '<leader>dT', ":lua require'dap'.clear_breakpoints()<CR>")
 vim.keymap.set('n', '<leader>dc', ":lua require'dap'.continue()<CR>")
@@ -23,6 +23,8 @@ vim.keymap.set('n', '<leader>dh', ":lua require'dap'.step_out()<CR>")
 vim.keymap.set('n', '<leader>dl', ":lua require'dap'.step_into()<CR>")
 vim.keymap.set('n', '<leader>dj', ":lua require'dap'.step_over()<CR>")
 vim.keymap.set('n', '<leader>db', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+-- TODO
+-- dap mappings for equivalent vimspector features
 -- nmap <leader>de :VimspectorEval
 -- nmap <leader>dw :VimspectorWatch
 -- nmap <leader>do :VimspectorShowOutput
@@ -32,35 +34,8 @@ vim.keymap.set('n', '<leader>db', ":lua require'dap'.set_breakpoint(vim.fn.input
 -- " for visual mode, the visually selected text
 -- xmap <Leader>di <Plug>VimspectorBalloonEval
 
--- mapping for 'K' to dap.ui.widgets.hover() while in debugging session
-local api = vim.api
-local keymap_restore = {}
-dap.listeners.after['event_initialized']['me'] = function()
-  for _, buf in pairs(api.nvim_list_bufs()) do
-    local keymaps = api.nvim_buf_get_keymap(buf, 'n')
-    for _, keymap in pairs(keymaps) do
-      if keymap.lhs == "K" then
-        table.insert(keymap_restore, keymap)
-        api.nvim_buf_del_keymap(buf, 'n', 'K')
-      end
-    end
-  end
-  api.nvim_set_keymap(
-    'n', 'K', '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
-end
-
-dap.listeners.after['event_terminated']['me'] = function()
-  for _, keymap in pairs(keymap_restore) do
-    api.nvim_buf_set_keymap(
-      keymap.buffer,
-      keymap.mode,
-      keymap.lhs,
-      keymap.rhs,
-      { silent = keymap.silent == 1 }
-    )
-  end
-  keymap_restore = {}
-end
+-- TODO
+-- add remapping of K
 
 -- virtual text for debugging
 require('nvim-dap-virtual-text').setup()
