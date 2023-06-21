7800007bb7fce9982721a7e7b6e0b0765fad2e0b
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2023-06-21 12:39:15 +0200

    refactor: refactored after install scripts

--- after_install_2.sh
+++ after_install_2.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 set -e
 
@@ -13,25 +13,4 @@ stow nvim tmux zsh i3 i3status alacritty
 # install tmux plugins
 ~/.tmux/plugins/tpm/bin/install_plugins
 
-# add core asdf plugins
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add python https://github.com/asdf-community/asdf-python.git
-asdf plugin add rust https://github.com/asdf-community/asdf-rust.git
-
-asdf install nodejs latest
-asdf global nodejs latest
-asdf install rust latest
-asdf global rust latest
-
-# install alacritty - depends on rust already being on the system
-cargo install alacritty
-
-# install yarn - needed for some neovim plugins
-npm install --global yarn
-
-# install neovim plugins
-# error output needs to redirected as there are plenty errors
-# after running neovim's config without plugins installed
-nvim --headless +PackerSync +qa &>/dev/null
-
-printf "\n\nRemember to set your font to Hack Nerd Font in terminal emulator.\n\n	You need to login again\n\n"
+printf "\n\n	You need to login again\n\n"
--- docker_install.sh
+++ docker_install.sh
@@ -11,8 +11,8 @@ sudo apt-get -y install ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 printf "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 printf "\n\nAdding current user to docker group.\n\n"
 sudo usermod -aG docker $USER
