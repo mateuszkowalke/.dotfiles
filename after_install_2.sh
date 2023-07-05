e219dd805dd2d4a2a1c43cf78ef617f1c4bcad06
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2023-07-05 20:44:53 +0200

    refactor: removed alacritty from config and other minor changes

--- after_install_2.sh
+++ after_install_2.sh
@@ -8,6 +8,6 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # stow configs
 rm ~/.zshrc
 cd ~/.dotfiles
-stow nvim tmux zsh i3 i3status alacritty
+stow nvim tmux zsh i3 i3status wezterm
 
 printf "\n\n	You need to login again\n\n"
