7a1313c86d26fa055dabdb1eb0c277689f48a019
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2023-07-05 20:46:05 +0200

    fix: added TS parsers required by lsp-saga

--- nvim/.config/nvim/lua/core/plugin-config/treesitter.lua
+++ nvim/.config/nvim/lua/core/plugin-config/treesitter.lua
@@ -1,5 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { 'javascript', 'typescript', 'json', 'css', 'html', 'go', 'rust', 'c' },
+    -- markdown and markdown_inline are required by lsp-saga
+    ensure_installed = { 'javascript', 'typescript', 'json', 'css', 'html', 'go', 'rust', 'c', 'markdown', 'markdown_inline' },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
