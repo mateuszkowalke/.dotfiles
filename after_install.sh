673e54af07e41bbfbeb44337c69716f85ffd26df
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2023-07-07 21:37:11 +0200

    refactor: moved from using dmenu to rofi

--- after_install.sh
+++ after_install.sh
@@ -21,7 +21,7 @@ sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_
 # fuse and libfuse2 are needed to support AppImages
 sudo apt-get update
 sudo apt-get -y install apt-transport-https i3-wm i3status i3lock \
-    git make libssl-dev curl wget zsh dmenu build-essential xautolock \
+    git make libssl-dev curl wget zsh rofi build-essential xautolock \
     stow fzf pip tmux lm-sensors brave-browser liferea pgadmin4 ripgrep \
     maim xclip xsel feh compton jq wireshark nmap gnome-clocks solaar \
     fuse libfuse2 gimp
