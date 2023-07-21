f6cab997a7a564dbb73005054892c8b075dd2c1c
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2023-07-21 19:38:41 +0200

    refactor: moved to using deb package for wezterm

--- wezterm_install.sh
+++ wezterm_install.sh
@@ -3,11 +3,10 @@
 set -e
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
-sudo cp Hack.zip /usr/share/fonts
-sudo unzip /usr/share/fonts/Hack.zip
+sudo mv Hack.zip /usr/share/fonts
+sudo unzip /usr/share/fonts/Hack.zip -d /usr/share/fonts
 sudo rm /usr/share/fonts/Hack.zip
 
-wget https://github.com/wez/wezterm/releases/download/20230408-112425-69ae8472/WezTerm-20230408-112425-69ae8472-Ubuntu20.04.AppImage
-mv WezTerm-20230408-112425-69ae8472-Ubuntu20.04.AppImage ~/Applications/wezterm
-chmod +x ~/Applications/wezterm
-rm -rf WezTerm-20230408-112425-69ae8472-Ubuntu20.04.AppImage
+wget https://github.com/wez/wezterm/releases/download/20230712-072601-f4abf8fd/wezterm-20230712-072601-f4abf8fd.Ubuntu20.04.deb
+sudo apt-get -y install ./wezterm-20230712-072601-f4abf8fd.Ubuntu20.04.deb
+rm -rf wezterm-20230712-072601-f4abf8fd.Ubuntu20.04.deb
