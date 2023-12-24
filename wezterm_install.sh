8aca4e23fb5065d4ca5e4f3d6bb94207d4dbed81
mateuszkowalke <mateusz.kowalke@gmail.com>
2023-12-24 20:50:45 +0100

    refactor: remove dead code in wezterm install script

--- wezterm_install.sh
+++ wezterm_install.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
-sudo mv Hack.zip /usr/share/fonts
-sudo unzip /usr/share/fonts/Hack.zip -d /usr/share/fonts
-sudo rm /usr/share/fonts/Hack.zip
-
 wget https://github.com/wez/wezterm/releases/download/20230712-072601-f4abf8fd/wezterm-20230712-072601-f4abf8fd.Ubuntu20.04.deb
 sudo apt-get -y install ./wezterm-20230712-072601-f4abf8fd.Ubuntu20.04.deb
 rm -rf wezterm-20230712-072601-f4abf8fd.Ubuntu20.04.deb
