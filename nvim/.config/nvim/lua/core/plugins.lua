f47ceb49c297108330589fe196a66f1f784b096b
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2024-01-25 11:04:10 +0100

    update telescope version

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -32,7 +32,7 @@ return require("packer").startup(function(use)
 	-- telescope
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
+		tag = "0.1.4",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
