96c7ede86834acb0dfe75b01f6dddaded2a40610
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2024-01-25 10:45:40 +0100

    move back to giving notes unique ids

--- nvim/.config/nvim/lua/core/plugin-config/obsidian.lua
+++ nvim/.config/nvim/lua/core/plugin-config/obsidian.lua
@@ -33,7 +33,7 @@ require("obsidian").setup({
 		-- Optional, if you want to change the date format of the default alias of daily notes.
 		alias_format = "%B %-d, %Y",
 		-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-		template = "daily.md",
+		template = 'daily.md',
 	},
 
 	-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
@@ -86,7 +86,20 @@ require("obsidian").setup({
 
 	-- Optional, customize how names/IDs for new notes are created.
 	note_id_func = function(title)
-		return title
+		-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+		-- In this case a note with the title 'My new note' will be given an ID that looks
+		-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+		local suffix = ""
+		if title ~= nil then
+			-- If title is given, transform it into valid file name.
+			suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+		else
+			-- If title is nil, just add 4 random uppercase letters to the suffix.
+			for _ = 1, 4 do
+				suffix = suffix .. string.char(math.random(65, 90))
+			end
+		end
+		return tostring(os.time()) .. "-" .. suffix
 	end,
 
 	-- Optional, boolean or a function that takes a filename and returns a boolean.
@@ -129,7 +142,7 @@ require("obsidian").setup({
 	follow_url_func = function(url)
 		-- Open the URL in the default web browser.
 		-- vim.fn.jobstart({ "open", url }) -- Mac OS
-		vim.fn.jobstart({ "xdg-open", url }) -- linux
+		vim.fn.jobstart({"xdg-open", url})  -- linux
 	end,
 
 	-- Optional, set to true if you use the Obsidian Advanced URI plugin.
