6bb13757fd72c9f635ab847189dc1afa007d2684
mateuszkowalke <mateusz.kowalke@gmail.com>
2024-02-04 16:14:03 +0100

    update wezterm

--- wezterm_install.sh
+++ wezterm_install.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-wget https://github.com/wez/wezterm/releases/download/20240128-202157-1e552d76/wezterm-20240128-202157-1e552d76.Ubuntu20.04.deb
-sudo apt-get -y install ./wezterm-20240128-202157-1e552d76.Ubuntu20.04.deb
-rm -rf wezterm-20240128-202157-1e552d76.Ubuntu20.04.deb
-
+wget https://github.com/wez/wezterm/releases/download/20240203-110809-5046fc22/wezterm-20240203-110809-5046fc22.Ubuntu22.04.deb
+sudo apt-get -y install ./wezterm-20240203-110809-5046fc22.Ubuntu22.04.deb
+rm -rf wezterm-20240203-110809-5046fc22.Ubuntu22.04.deb
