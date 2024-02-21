af777e62574b7fd0451019fc04ae9a6e80f85bdd
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2024-02-21 19:37:50 +0100

    refactor: cmp refactor wip

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -44,11 +44,10 @@ return require("packer").startup(function(use)
 	-- Completion framework:
 	use("hrsh7th/nvim-cmp")
 
-	-- LSP completion source:
+	-- completion sources:
 	use("hrsh7th/cmp-nvim-lsp")
-
-	-- Useful completion sources:
-	use("hrsh7th/cmp-nvim-lsp-signature-help")
+    use("hrsh7th/cmp-nvim-lsp-signature-help")
+	use("hrsh7th/cmp-nvim-lua")
 	use("hrsh7th/cmp-path")
 	use("hrsh7th/cmp-buffer")
 	use("hrsh7th/cmp-cmdline")
