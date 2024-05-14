204c810949d9aa141759ca9db028cb163831b568
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2024-05-14 08:30:53 +0200

    keybindings for nvim and tmux

--- nvim/.config/nvim/lua/core/remaps.lua
+++ nvim/.config/nvim/lua/core/remaps.lua
@@ -1,21 +1,32 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+local set = vim.keymap.set
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
+set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
 
 -- these allow for moving selection up or down
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- up and down movements and moving to search results
 -- stay in the middle of screen
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
 
 -- copying to system clipboard
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>Y', '"+Y')
+set("n", "<leader>y", '"+y')
+set("v", "<leader>y", '"+y')
+set("n", "<leader>Y", '"+Y')
 
 -- escaping terminal mode
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+set("t", "<Esc>", "<C-\\><C-n>")
+
+-- resizing panes
+set("n", "<M-,>", "<c-w>5<")
+set("n", "<M-.>", "<c-w>5>")
+set("n", "<M-t>", "<c-w>+")
+set("n", "<M-s>", "<c-w>-")
