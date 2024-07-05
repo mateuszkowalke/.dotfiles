a800d3a96e0de501de9e461d6359a2648dd214ae
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2024-07-05 09:13:06 +0200

    setup a keybinding for opening neogit

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -8,5 +8,11 @@ return {
 		"nvim-telescope/telescope.nvim", -- optional
 		"ibhagwan/fzf-lua", -- optional
 	},
-	config = true,
+	config = function()
+		local neogit = require("neogit")
+        neogit.setup()
+		vim.keymap.set("n", "<leader>ng", function()
+			neogit.open()
+		end)
+	end,
 }
