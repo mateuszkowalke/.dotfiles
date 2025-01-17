4912237a8a9326c94a0df5be373faf8569b847ff
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2025-01-17 20:46:10 +0100

    add additional git config and alias

--- git_aliases.sh
+++ git_aliases.sh
@@ -5,3 +5,6 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.lg 'log --all --decorate --oneline --graph'
+git config --global rerere.enabled true
+# undo the number of changes to current branch (by default undoes one change)
+git config --global alias.undo '!f() {git reset --hard $(git rev-parse --abbrev-ref HEAD)@{${1-1}}; }; f'
